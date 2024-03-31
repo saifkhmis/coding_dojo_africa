e658a9105960adeaeedbc4d0defb8d3151bc6fe5
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-04-01 00:14:56 +0100

    portfolio assignment

--- Python/Mysql/core/dojo_ninjas/dojo_ninjas.sql
+++ Python/Mysql/core/dojo_ninjas/dojo_ninjas.sql
@@ -10,11 +10,11 @@ value('ahmed','khmis',20, 4),('saif','khmis',21,4),('hala','khmis',31,4),
 ('naziha','khmis',20, 5),('taher','khmis',21,5),('hajer','khmis',31,5),
 ('salem','khmis',20, 6),('islem','khmis',21,6),('howaida','khmis',31,6);
 select * from dojos
-left join ninjas on dojo.id=ninjas.dojo_id
+join ninjas on dojo.id=ninjas.dojo_id
 where dojo_id=4 ;
 select * from dojos
-left join ninjas on dojo.id=ninjas.dojo_id
+join ninjas on dojo.id=ninjas.dojo_id
 where dojo_id=6;
 select * from dojos 
-left join ninjas on dojo.id=ninjas.dojo_id
+join ninjas on dojo.id=ninjas.dojo_id
 where dojo_id=5;
