3b69be12a726605d4edf194f3b6a49677b962166
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-05-14 18:23:14 +0100

    portfolio assignment

---
+++ Javascript/javascript/optional/object_master/object.js
@@ -0,0 +1,50 @@
+const pokémon = Object.freeze([
+        { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+        { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+        { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+        { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+        { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+        { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+        { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+        { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+        { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+        { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+        { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+        { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+        { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+        { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+        { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+        { "id": 98,  "name": "Krabby",     "types": ["water"] },
+        { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+        { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+        { "id": 133, "name": "Eevee",      "types": ["normal"] },
+        { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+        { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+        { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+        { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+    ]);
+const pokémonDivisibleByThree = pokémon.filter(pokémon => pokémon.id % 3 === 0);
+console.log(pokémonDivisibleByThree);
+const bListPkmn = pokémon.filter( p => p.types[0] === "fire" );
+console.log(bListPkmn);
+const saif = pokémon.filter(pokémon => pokémon.types.length > 1);
+console.log(saif);
+const pokémonNames = pokémon.map(pokémon => pokémon.name);
+console.log(pokémonNames);
+const pokémonNamesWithIdGreaterThan99 = pokémon
+ .filter(pokémon => pokémon.id > 99)
+ .map(pokémon => pokémon.name);
+ console.log(pokémonNamesWithIdGreaterThan99);
+
+const pokémonNamesWithOnlyPoisonType = pokémon
+.filter(pokémon => pokémon.types.length === 1 && pokémon.types[0] === "poison")
+.map(pokémon => pokémon.name);
+console.log(pokémonNamesWithOnlyPoisonType);
+const firstTypesOfPokémonWithFlyingType = pokémon
+.filter(pokémon => pokémon.types.length > 1 && pokémon.types.includes("flying"))
+.map(pokémon => pokémon.types[0]);
+console.log(firstTypesOfPokémonWithFlyingType);
+const normalTypePokémonCount = pokémon
+.filter(pokémon => pokémon.types.includes("normal"))
+.length;
+console.log(normalTypePokémonCount);
