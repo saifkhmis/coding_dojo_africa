c2abfea744e7ed63e2579fa2b87bf2e0f5443ab3
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-06-01 23:28:35 +0100

    prop it up

--- Javascript/React/Practice/prop_it_up/prop-it-up/src/App.js
+++ Javascript/React/Practice/prop_it_up/prop-it-up/src/App.js
@@ -2,9 +2,19 @@ import logo from './logo.svg';
 import PersonCard from './components/PersonCard';
 import './App.css';
 
+
+// const first_name =['doe','smith', 'fillmore','smith']
+// const last_name = ['john','jane','jane','jane']
+// const age = [45, 88, 50, 62]
+// const hair_color = ['brown','black','brown','brown']
 function App() {
   return (
-      <PersonCard/>
+    <>
+    <PersonCard firstname={'Doe'} lastname={'john'} age={45} />
+    <PersonCard firstname={'Smith'} lastname={'jane'} age={88}/>
+    <PersonCard firstname={"Fillmore"} lastname={'jane'} age={50} />
+    <PersonCard firstname={'Smith'} lastname={'jane'} age={62} />
+    </>
   );
 }
 
--- Javascript/React/Practice/prop_it_up/prop-it-up/src/components/PersonCard.jsx
+++ Javascript/React/Practice/prop_it_up/prop-it-up/src/components/PersonCard.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
 
-const PersonCard = () => {
-    const first_name =['doe','smith', 'fillmore','smith']
-    const last_name = ['john','jane','jane','jane']
-    const age = [45, 88, 50, 62]
-    const hair_color = ['brown','black','brown','brown']
-    const [doe , smit , fillmore,smith]=first_name;
-    const [jo,ja,jann,naa]=last_name;
-    const [one,tow,th,fr]=age;
-    const[br,bl,bw,brw]=hair_color;
+const PersonCard = (props) => {
+
   return (
-    <div><h1>{doe}, {jo}</h1>
-    <p>Age:{one}</p>
-    <p>Hair color: {br}</p>
-    <h1>{smit}, {ja}</h1>
-    <p>Age:{tow}</p>
-    <p>Hair color: {bl}</p>
-    <h1>{fillmore}, {jann}</h1>
-    <p>Age:{th}</p>
-    <p>Hair color: {bw}</p>
-    <h1>{smith}, {naa}</h1>
-    <p>Age:{fr}</p>
-    <p>Hair color: {brw}</p>
+    <div><h1>{props.firstname}, {props.lastname}</h1>
+    <p>Age:{props.age}</p>
+    <p>Hair color: {props.hair}</p>
     </div>
+
     
     
   )
