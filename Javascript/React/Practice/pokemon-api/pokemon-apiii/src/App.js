bc13bcaa7206f9e30abb450df1deed68b4d1792c
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-06-04 21:18:27 +0100

    pokemon api

---
+++ Javascript/React/Practice/pokemon-api/pokemon-apiii/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Api } from './componentes/Api';
+
+function App() {
+  return (
+    <div className="App">
+      <Api/>
+    </div>
+  );
+}
+
+export default App;
