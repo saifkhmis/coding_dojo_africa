38890c580040ef4d0ae00bbfd372d1d336cd9350
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-10-02 11:44:33 +0100

    first react

---
+++ Javascript/React/core/First-jsx/first/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello dojo !</h1>
+      <ul>Things I need to do :</ul>
+      <li>Learn React</li>
+      <li>Climb Mt.Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
