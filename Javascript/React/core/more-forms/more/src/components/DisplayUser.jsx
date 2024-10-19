4d01ae29049eadc3f2bd1ac3e8adeabf4e9b4466
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-10-19 19:30:58 +0100

    more form

---
+++ Javascript/React/core/more-forms/more/src/components/DisplayUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DisplayUser = (props) => {
+    const {user}=props
+  return (
+    <div><div>DisplayUser</div>
+        </div>
+
+  )
+}
+
+export default DisplayUser
