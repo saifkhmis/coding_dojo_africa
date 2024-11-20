a4744c59e549a2d36471d66ab3188787f6e5a1fb
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2024-11-20 21:21:38 +0100

    first java

---
+++ Java/java Fun/Practise/First java/First.java
@@ -0,0 +1,7 @@
+public class First {
+	public static void main(String[] args) {
+        System.out.println("My name is saif islem khmis");
+        System.out.println("I am 20 years old ");
+        System.out.println("My hometown is mehdia ");
+	}
+}
