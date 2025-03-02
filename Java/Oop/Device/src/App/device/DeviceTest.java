12a582c4ae53c2c7834b286f3bdd950253ce6c1c
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2025-03-03 00:21:48 +0100

    device

---
+++ Java/Oop/Device/src/App/device/DeviceTest.java
@@ -0,0 +1,22 @@
+package App.device;
+
+public class DeviceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Phone phone = new Phone();
+		phone.makeCall();
+		phone.makeCall();
+		phone.makeCall();
+		phone.playGame();
+		phone.playGame();
+		phone.charge();
+		phone.playGame();
+		phone.playGame();
+		phone.playGame();
+		phone.playGame();
+		phone.playGame();
+		phone.playGame();
+	}
+
+}
