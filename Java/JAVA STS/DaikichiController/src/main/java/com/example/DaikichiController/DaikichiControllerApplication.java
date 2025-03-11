5531c17ff44a3b7dfd55e6a53ae92609ff2553ab
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2025-03-11 03:55:56 +0100

    Daikichi routes

---
+++ Java/JAVA STS/DaikichiController/src/main/java/com/example/DaikichiController/DaikichiControllerApplication.java
@@ -0,0 +1,14 @@
+package com.example.DaikichiController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DaikichiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiControllerApplication.class, args);
+	}
+
+}
+
+
---
+++ Java/JAVA STS/DaikichiController/src/test/java/com/example/DaikichiController/DaikichiControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DaikichiController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
