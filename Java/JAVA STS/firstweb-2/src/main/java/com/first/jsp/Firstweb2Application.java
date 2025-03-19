4269ebca513ba8e5589db519b09289c0f2671311
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2025-03-19 22:23:57 +0100

    Hopper receipt

---
+++ Java/JAVA STS/firstweb-2/src/main/java/com/first/jsp/Firstweb2Application.java
@@ -0,0 +1,13 @@
+package com.first.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstweb2Application.class, args);
+	}
+
+}
---
+++ Java/JAVA STS/firstweb-2/src/test/java/com/first/jsp/Firstweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firstweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
